223090671ac3909b5d0eaeb6a1eee17eb437902f
Ellekwis <43903852+Ellekwis@users.noreply.github.com>
2018-10-15 18:04:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Артём
+ * Date: 20.09.2018
+ * Time: 14:28
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace np2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int c;
+			if (int.TryParse(Console.ReadLine(), out c)==true){
+				Console.WriteLine("Это число");}
+			else {Console.WriteLine("Это не число");}
+			Console.ReadLine();
+			}
+		}
+	}
+
+
+		      
+			
+		
+				
+			 
+		
+ 
+
